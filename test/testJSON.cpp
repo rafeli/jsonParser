232305d02e129113327f00cce9fcf12a539d132b
rafel <rafelisraels@gmail.com>
2018-04-14 18:50:06 +0200

    parsing string ok, one more test

--- test/testJSON.cpp
+++ test/testJSON.cpp
@@ -34,7 +34,7 @@ int TestJSON::testParse() {
   // -0- Entering
   MYLOG(DEBUG,"ENTER");
 
-  // -1- int positive and negative
+  // -1- integer
   try {
 
     test_ = "integer";
@@ -59,6 +59,7 @@ int TestJSON::testParse() {
 
   // -1a- incorrect integers
   try {
+    test_ = "incorrect integer";
     testString = "1 1";
     jsValue = json.parse(testString);
     momo::TestTools::report(false, "didn't recognize as error: 1 1"); 
@@ -86,17 +87,23 @@ int TestJSON::testParse() {
     momo::TestTools::report(jsValue.stringify(), "-1.2e-05", test_); 
     numTests++;
 
+    testString = "-6.022300e+23";
+    jsValue = json.parse(testString);
+    momo::TestTools::report(jsValue.stringify(), "-6.022300e+23", test_); 
+    numTests++;
+
   } catch (std::string s) {
     std::cout << "ERROR in " << test_ <<": " << s;
   }
 
   // -2a- incorrect doubles
   try {
+    test_ = "incorrect double";
     testString = "A1";
     jsValue = json.parse(testString);
     momo::TestTools::report(false, "didn't recognize as error: A1"); 
   } catch (std::string e) {
-    momo::TestTools::report(e, "??", test_); 
+    momo::TestTools::report(e, "Error parsing number from: \"A1\" expecting digit, not: A1", test_); 
     numTests++;
   }
 
@@ -105,7 +112,7 @@ int TestJSON::testParse() {
     jsValue = json.parse(testString);
     momo::TestTools::report(false, "didn't recognize as error: A1"); 
   } catch (std::string e) {
-    momo::TestTools::report(e, "??", test_); 
+    momo::TestTools::report(e, "Error parsing number from: \"1.A1\" expecting digit, not: A1", test_); 
     numTests++;
   }
 
@@ -114,94 +121,28 @@ int TestJSON::testParse() {
     jsValue = json.parse(testString);
     momo::TestTools::report(false, "didn't recognize as error: A1"); 
   } catch (std::string e) {
-    momo::TestTools::report(e, "??", test_); 
+    momo::TestTools::report(e, "Error parsing number from: \"1.2E\" expecting digit, not: ", test_); 
     numTests++;
   }
 
-//
-//  // -3- just a string
-//  try {
-//    actual_.str("");
-//    expected_ = "\"12abc34\"";
-//    testString = "\"12abc34\"";
-//    driver.parse(testString);
-//    actual_ << driver.result; 
-//    momo::TestTools::report(actual_.str(), expected_, "just a string"); 
-//    MYLOG(DEBUG,"Test String complete");
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//  // -4- single quoted string
-//  try {
-//    actual_.str("");
-//    expected_= testString =  "\"12abc34\"";
-//    driver.parse(testString);
-//    actual_ << driver.result; 
-//    momo::TestTools::report(actual_.str(), expected_, "a single quoted string"); 
-//    MYLOG(DEBUG,"Test Single Quoted String complete");
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//  // -5- string with \t and \n and \"
-//  //     changed to \\t and \\n, as these chars are encoded in JSON
-//  try {
-//    testString =     "\"12\tabc\\\"def\n34\"";
-//    expected_=    "12\tabc\"def\n34";
-//    driver.parse(testString);
-//    momo::TestTools::report(driver.result.getString(), expected_, "single quoted string containing tab and cr"); 
-//    MYLOG(DEBUG,"Test Single Quoted String complete");
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//
-//
-//  // -6- double in scientific format
-//  test_ = "parseDouble scientific format";
-//  try {
-//    actual_.str("");
-//    testString = "6.022300e-23";
-//    driver.parse(testString);
-//    momo::TestTools::report(driver.result.stringify(), testString, test_); 
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//  // -7- double in scientific format
-//  test_ = "parseDouble negative ";
-//  try {
-//    actual_.str("");
-//    testString = "-6.022300e+23";
-//    driver.parse(testString);
-//    momo::TestTools::report(driver.result.stringify(), testString, test_); 
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//  // -8- double in fixed format: precision set to LOWPRECISION = 2
-//  test_ = "parseDouble fixed format";
-//  try {
-//    testString = "17.0500";
-//    driver.parse(testString);
-//    momo::TestTools::report(driver.result.stringify(), "1.70500e+01", test_); 
-//    MYLOG(DEBUG,"Test double complete");
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
-//  // -9- double in non-standard format: precision set to LOWPRECISION = 2
-//  test_ = "parseDouble non-standard format";
-//  try {
-//    testString = "16.000e-3";
-//    driver.parse(testString);
-//    momo::TestTools::report(driver.result.stringify(), "1.60e-02", test_); 
-//    MYLOG(DEBUG,"Test double complete");
-//  } catch (std::string s) {
-//    std::cout << "ERROR in " << test_ <<": " << s << std::endl;
-//  }
-//
+  // -3- string
+  try {
+
+    test_ = "string";
+    testString = "\"12abc34\"";
+    jsValue = json.parse(testString);
+    momo::TestTools::report(jsValue.getString(), testString, test_); 
+    numTests++;
+
+    testString =     "\"12\tabc\\\"def\n34\"";
+    jsValue = json.parse(testString);
+    momo::TestTools::report(jsValue.getString(), testString, test_); 
+    numTests++;
+
+  } catch (std::string s) {
+    std::cout << "ERROR in " << test_ <<": " << s;
+  }
+
 //  // -10 a simple array
 //  test_ = "parseArray";
 //  try {
